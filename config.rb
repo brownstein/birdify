c3ea97efbd8b5186a914c88a18c1696da34a5121
Robert Brownstein <robert.brownstein@gmail.com>
2014-10-20 20:07:20 -0400

    Flew project into Github

---
+++ config.rb
@@ -0,0 +1,6 @@
+# Birdify Compass definitions - use compass to build better birds today!
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
